2171e86398dd9c1f80f76c5c1b4b64f47e55d317
ps-astangl <astangl@leaporbit.com>
2021-04-22 08:30:41 -0500

    Added ui

--- src/SamlTester/ClientApp/src/app/app.module.ts
+++ src/SamlTester/ClientApp/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AssertionBoxComponent } from './assertion-box/assertion-box.component';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssertionBoxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
